7d59acebca3349197bc98f2cdf625a99b9cb7198
APAC\BAE1KOR <Abhilas.Banerjee@in.bosch.com>
2024-01-08 12:33:22 +0530

    My First Git commit

---
+++ learn-springboot-framework/src/main/java/com/springboot/selflearning/learnspringbootframework/LearnSpringbootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.selflearning.learnspringbootframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootFrameworkApplication.class, args);
+	}
+
+}
---
+++ learn-springboot-framework/src/main/java/com/springboot/selflearning/learnspringbootframework/controller/CourseController.java
@@ -0,0 +1,29 @@
+package com.springboot.selflearning.learnspringbootframework.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.selflearning.learnspringbootframework.controller.model.Course;
+
+@RestController
+public class CourseController {
+	
+//	[{
+//		"id":1,
+//		"name": "Learn AWS",
+//		"author": "in28minutes"
+//	}]
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1,"Learn AWS","in 28minutes"),
+				new Course(2,"Learn Azure1","in 28minutes"),
+				new Course(3,"Learn DevOps","in 28minutes")
+				);
+	}
+
+}
